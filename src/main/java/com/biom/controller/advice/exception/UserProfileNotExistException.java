175d788ce53af1c77f5bbc67c92a26b9b790b1a2
DaxaA <stud.arhpva@gmail.com>
2022-09-30 16:01:16 +0400

    deleted old files

--- src/main/java/com/biom/controller/advice/exception/UserProfileNotExistException.java
+++
@@ -1,7 +0,0 @@
-package com.biom.controller.advice.exception;
-
-public class UserProfileNotExistException extends RuntimeException{
-    public UserProfileNotExistException(String message) {
-        super(message);
-    }
-}
