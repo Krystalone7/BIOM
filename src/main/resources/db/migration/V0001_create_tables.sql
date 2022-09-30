c9fb663579e81640943830ab03333558710acbae
DaxaA <stud.arhpva@gmail.com>
2022-09-30 16:02:51 +0400

    database migration for creating tables

--- src/main/resources/db/migration/V0001_create_tables.sql
+++ src/main/resources/db/migration/V0001_create_tables.sql
@@ -1,12 +1,26 @@
-DROP TABLE IF EXISTS user_profile;
+drop table if exists biom.user;
+drop table if exists biom.roles;
+drop table if exists biom.user_roles;
 
-create table user_profile
+
+create table biom.user
+(
+    id        bigserial primary key,
+    username varchar(25) not null,
+    name      varchar(25) not null,
+    surname   varchar(25) not null,
+    birthdate date,
+    info      varchar(200),
+    hobbies   varchar(200),
+    phone     varchar(25),
+    email     varchar(100),
+    password  varchar(25) not null,
+    role_id integer not null
+);
+create table biom.roles
 (
     id bigserial primary key,
-    name varchar(25) not null,
-    surname varchar(25) not null,
-    birthdate date not null,
-    info varchar(200) not null,
-    hobbies varchar(200) not null,
-    phone varchar(25) not null
+    name varchar(25) not null
 );
+
+insert into biom.user(username, name, surname, password, role_id) values('user', 'darya', 'a', '12345', 1);
