9e8fbdd060627def8e617f589bd9dbaeabb07676
DaxaA <stud.arhpva@gmail.com>
2022-10-07 19:09:12 +0400

    deleted

--- src/main/java/com/biom/entity/ERole.java
+++
@@ -1,7 +0,0 @@
-package com.biom.entity;
-
-public enum ERole {
-    USER,
-    MODERATOR,
-    ADMIN
-}
--- src/main/java/com/biom/service/SecurityService.java
+++
@@ -1,6 +0,0 @@
-/*package com.biom.service;
-
-public interface SecurityService {
-    String findLoggedInUsername();
-    void autoLogin(String username, String password);
-}*/
