aebe3dfdd64d9c0182cd4b09df7f68b27146f95a
DaxaA <stud.arhpva@gmail.com>
2022-10-09 16:02:59 +0400

    дописала эту штуку

--- src/main/java/com/biom/service/impl/JpaUserService.java
+++ src/main/java/com/biom/service/impl/JpaUserService.java
@@ -2,18 +2,38 @@ package com.biom.service.impl;
 
 import com.biom.dto.UserCreateDto;
 import com.biom.dto.UserDto;
+import com.biom.entity.User;
+import com.biom.repository.UserRepository;
 import com.biom.service.UserService;
+import com.biom.service.mapper.UserListMapper;
+import com.biom.service.mapper.UserMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
+@RequiredArgsConstructor
 public class JpaUserService implements UserService {
+    private final UserRepository userRepository;
+    private final UserMapper userMapper;
+    private final UserListMapper userListMapper;
+
     @Override
     public UserDto createUser(UserCreateDto userCreateDto) {
-        return null;
+        User user = new User(
+                userCreateDto.getEmail(),
+                userCreateDto.getPassword()
+        );
+
+        user = userRepository.saveAndFlush(user);
+
+        return userMapper.toDto(user);
     }
 
     @Override
     public List<UserDto> getAllUser() {
-        return null;
+        List<User> users = userRepository.findAll();
+        return userListMapper.toDtoList(users);
     }
 }
