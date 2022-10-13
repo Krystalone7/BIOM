db02bb02c32a6efbbfd5ae8a97400c08a2c2a6e0
DaxaA <stud.arhpva@gmail.com>
2022-10-13 18:08:40 +0400

    resources

--- src/main/resources/db/migration/V0001_create_tables.sql
+++ src/main/resources/db/migration/V0001_create_tables.sql
@@ -1,20 +1,13 @@
-drop table if exists biom.user;
-drop table if exists biom.role;
 drop table if exists biom.user_role;
+drop table if exists biom.role;
+drop table if exists biom.user;
 
 
 create table biom.user
 (
     id        bigserial primary key,
-    username varchar(25) not null,
-    name      varchar(25) not null,
-    surname   varchar(25) not null,
-    birthdate date,
-    info      varchar(200),
-    hobbies   varchar(200),
-    phone     varchar(25),
-    email     varchar(100),
-    password  varchar(25) not null
+    email     varchar(100) not null,
+    password  varchar(100) not null
 );
 create table biom.role
 (
@@ -25,9 +18,9 @@ create table biom.user_role
 (
     id serial primary key,
     user_id bigserial not null
-        references biom.user (id),
+        references biom.user(id),
     role_id bigserial not null
-        references biom.role (id)
+        references biom.role(id)
 );
 insert into biom.role
 values
